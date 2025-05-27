c247c73d75b4b4e67207dc382758ce884c75cdbc
tejas-waghamare <tejaswaghamare121@gmail.com>
2025-05-27 22:28:56 +0530

    second commit

--- frontend-gemini/src/components/Sidebar.jsx
+++ frontend-gemini/src/components/Sidebar.jsx
@@ -9,12 +9,12 @@ const Sidebar = () => {
       <div className='bg-black w-1/5 overflow-y-scroll p-5 text-white'>
          <div className=''>
             <Link to="/new-chat">
-               <div className='hover:bg-gray-700 cursor-pointer text-2xl p-2 font-bold'>New Chat </div>
+               <div className='hover:bg-gray-700 cursor-pointer rounded-2xl text-2xl p-2 font-bold'>New Chat </div>
             </Link>
             {
                chats.map(c => (
                   <Link to={`${c._id}`} key={c._id}>
-                     <div className='hover:bg-gray-700 cursor-pointer p-2'>
+                     <div className='hover:bg-gray-700 rounded-2xl cursor-pointer p-2'>
                         {c.name}
                      </div>
                   </Link>
