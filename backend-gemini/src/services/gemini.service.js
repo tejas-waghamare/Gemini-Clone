e552500b620e1caef3746d13dfeeaacd722042cb
tejas-waghamare <tejaswaghamare121@gmail.com>
2025-12-29 17:07:54 +0530

    initial commit

--- backend-gemini/src/services/gemini.service.js
+++ backend-gemini/src/services/gemini.service.js
@@ -12,7 +12,7 @@ const ai = new GoogleGenAI({ apiKey: GEMINI_API_KEY });
 export const getGeminiResponse = async (question) => {
     try {
         const response = await ai.models.generateContent({
-            model: "gemini-1.5-flash",
+            model: "gemini-2.5-flash",
             contents: [{ role: "user", parts: [{ text: question }] }],
         });
 
